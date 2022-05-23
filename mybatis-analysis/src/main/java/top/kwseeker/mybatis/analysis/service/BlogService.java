13bbd78541708e5bcc25116e99ea54525db0441f
kwseekerli <kwseekerli@welove-inc.com>
2022-05-23 19:08:20 +0800

    temp

--- mybatis-analysis/src/main/java/top/kwseeker/mybatis/analysis/service/BlogService.java
+++
@@ -1,8 +0,0 @@
-package top.kwseeker.mybatis.analysis.service;
-
-import top.kwseeker.mybatis.analysis.domain.Blog;
-
-public interface BlogService {
-
-    Blog getBlogById(int id);
-}
--- mybatis-analysis/src/test/java/top/kwseeker/mybatis/analysis/service/BlogServiceImplTest.java
+++ mybatis-analysis/src/test/java/top/kwseeker/mybatis/analysis/service/BlogServiceImplTest.java
@@ -1,22 +1,35 @@
 package top.kwseeker.mybatis.analysis.service;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import top.kwseeker.mybatis.analysis.dao.BlogMapper;
 import top.kwseeker.mybatis.analysis.domain.Blog;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BlogServiceImplTest {
 
-    private static BlogService blogService;
+    private static final String CONFIG_XML = "mybatis-config.xml";
+
+    private static SqlSession sqlSession;
 
     @BeforeClass
-    public static void beforeClass() {
-        blogService = new BlogServiceImpl();
+    public static void beforeClass() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(CONFIG_XML);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSession = sqlSessionFactory.openSession(true);
     }
 
     @Test
     public void getBlogById() {
-        Blog blog = blogService.getBlogById(1);
+        BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
+        Blog blog = blogMapper.selectBlog(1);
         System.out.println("Query Result: " + blog.toString());
         Assert.assertEquals("kwseeker", blog.getNickname());
     }
