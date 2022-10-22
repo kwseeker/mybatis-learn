6a3b89518b8ca859b81186fad992f6fd88ee01ed
kwseeker <xiaohuileee@gmail.com>
2022-10-22 14:36:52 +0800

    Exception: Invalid bound statement 修复

--- mybatis-tk/tk-01-biz/src/test/java/top/kwseeker/mybatis/tk/biz/service/LoginServiceTest.java
+++ mybatis-tk/tk-01-biz/src/test/java/top/kwseeker/mybatis/tk/biz/service/LoginServiceTest.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -17,5 +20,14 @@ public class LoginServiceTest {
     @Test
     public void login() {
         loginService.login();
+    }
+
+    @Test
+    public void loadResource() throws IOException {
+        Enumeration<?> resourceUrls = this.getClass().getClassLoader().getResources("mapper/primary/");
+        while(resourceUrls.hasMoreElements()) {
+            URL url = (URL)resourceUrls.nextElement();
+            System.out.println(url.getPath());
+        }
     }
 }
